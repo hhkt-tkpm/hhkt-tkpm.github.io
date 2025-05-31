655251a9a5809eae927681a832124c7b164e571f
Nguyễn Anh Hào <anhhao012004@gmail.com>
2025-05-31 10:16:40 +0700

    Update Docusaurus configuration for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://hhkt-tkpm.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'hhkt-tkpm', // Usually your GitHub org/user name.
+  projectName: 'hhkt-tkpm.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // The branch that GitHub pages will deploy from.
+  trailingSlash: false, // Whether to add a trailing slash to URLs
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
